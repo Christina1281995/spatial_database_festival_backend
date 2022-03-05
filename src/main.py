45781e3c87938b87bc7d7108cb18b76c4936c4e5
Christina Zorenboehmer <81073205+Christina1281995@users.noreply.github.com>
2022-03-05 10:33:20 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,35 @@
-import sys
-import psycopg2
 import time
-import tkinter as tk
-from tkintermapview import TkinterMapView
-import pandas as pd
-import os
 import functions
 
 if __name__ == '__main__':
 
-    # Connect to DB
-    con = functions.connect("localhost", "festival", "postgres", "xxxxxxx")
-    # Check for PostGIS extension, check if tables exist -> if not then set up tables and add data from GitHub
+    # Get connection parameters
+    host, database, user, password = functions.connection_params()
+
+    # Connect to database via psycopg2
+    con = functions.connect(host, database, user, password)
+
+    # Set up database (PostGIS extension, tables, data, prepare plans)
     functions.setup(con)
 
     # Chill for a second
     time.sleep(1)
-    # Get user coordinates and the task number from user
+
+    # Guide user through decision process -> return task number and user position if required
     userY, userX, task = functions.decide()
 
-    # If there is no user location given -> pass
-    # Else update user_location table in postgres
-    if userY == 0:
-        pass
-    else:
-        # clear old position
-        remove = "DELETE FROM user_location;"
-        functions.sql_in(con, remove)
-        # add new position
-        add_user_pos = "INSERT INTO user_location(id, geom) VALUES(1, ST_GeomFromText('Point(%s %s)', 4326));" % (userX, userY)
-        functions.sql_in(con, add_user_pos)
-
-    # Prepare plans in database
-    functions.prepare_plans(con)
+    # Update user's position in the database
+    functions.update_position(userY, userX, con)
 
     # Perform queries, updates, etc. according the given task number and with all necessary user input
-    map, args = functions.perform_task(con, task)
+    show_map, args = functions.perform_task(con, task)
 
-    # Close the connection to the db
+    # Close the connection to the database
     con.close()
 
     print("\nTask complete. See you next time!")
-    # args = {"festival_area" : "festival_area", "main_stage": "101"}
-    if map == True:
+
+    # Lastly, if map is set to True show the user a map with the given arguments
+    if show_map:
         time.sleep(2)
-        functions.map(userY, userX, args)
-
-
+        functions.show_map(userY, userX, args)
